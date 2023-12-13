40b026a5b0bf7f9cb80b5fbcd1276b54e5c808a8
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2023-12-13 23:19:09 +0530

    fetching and converting to pdf

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-console.log("hey");
+import { Builder, By, until } from "selenium-webdriver";
+import puppeteer from "puppeteer";
+async function test(pageNumber) {
+  const browser = await puppeteer.launch({ headless: false });
+  try {
+    const page = await browser.newPage();
+    for (var i = 1; ; i++) {
+      await scrapPage(i, page);
+    }
+  } finally {
+  }
+}
+
+async function scrapPage(pageNo, page) {
+  await page.goto("https://www.examtopics.com/exams/amazon/*/view/" + pageNo, {
+    waitUntil: "networkidle2",
+  });
+  const captchaSelector = "captcha-container";
+  const captchBox = await page.$(`.${captchaSelector}`);
+  if (captchBox) {
+    console.log("captcha exists , solve it");
+    await page.waitForTimeout(60000);
+    console.log("wait done , let move");
+  }
+  const elementClassName = "reveal-solution";
+  await page.waitForSelector(`.${elementClassName}`);
+  const elements = await page.$$(`.${elementClassName}`);
+  console.log(elements);
+  for (const element of elements) {
+    await element.click();
+  }
+  const elementsToRemove = [
+    ".full-width-header",
+    ".alert-success",
+    ".alert-info",
+    ".rs-footer",
+  ];
+  // Remove specific elements from the page
+  await Promise.all(
+    elementsToRemove.map((selector) =>
+      page.$$eval(selector, (elements) => elements.forEach((el) => el.remove()))
+    )
+  );
+  await page.pdf({
+    path: "page1.pdf",
+    format: "A4",
+  });
+}
+
+async function waitForCaptcha(page) {
+  let timeout = 0;
+  while (timeout < 70000) {
+    const captchBox = await page.$(`.${captchaSelector}`);
+    if (!captchBox) {
+      console.log("captcha solved");
+      break;
+    } else {
+      y;
+    }
+  }
+}
+test();
